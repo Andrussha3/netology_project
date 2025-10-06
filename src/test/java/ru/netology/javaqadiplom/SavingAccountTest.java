44e54799a390df19065bb5ed36ef3c72c070605d
Андрей <lupolenkoandre@gmail.com>
2025-10-06 17:32:46 +0300

    Update SavingAccountTest.java

--- src/test/java/ru/netology/javaqadiplom/SavingAccountTest.java
+++ src/test/java/ru/netology/javaqadiplom/SavingAccountTest.java
@@ -18,38 +18,4 @@ public class SavingAccountTest {
 
         Assertions.assertEquals(2_000 + 3_000, account.getBalance());
     }
-    @Test
-    public void shouldPayWhenBalanceBecomesEqualToMinBalance() {
-        SavingAccount account = new SavingAccount(
-                2_000,
-                1_000,
-                10_000,
-                5
-        );
-
-        // Оплата 1000 рублей должна пройти успешно,
-        // так как баланс станет равным minBalance (1000)
-        boolean result = account.pay(1_000);
-
-        Assertions.assertTrue(result, "Оплата должна пройти успешно когда баланс становится равным minBalance");
-        Assertions.assertEquals(1_000, account.getBalance());
-    }
-
-    @Test
-    public void shouldNotPayWhenBalanceBecomesLessThanMinBalance() {
-        SavingAccount account = new SavingAccount(
-                2_000,
-                1_000,
-                10_000,
-                5
-        );
-
-        // Оплата 1001 рублей должна быть отклонена,
-        // так как баланс станет меньше minBalance (999 < 1000)
-        boolean result = account.pay(1_001);
-
-        Assertions.assertFalse(result, "Оплата должна быть отклонена когда баланс становится меньше minBalance");
-        Assertions.assertEquals(2_000, account.getBalance());
-    }
-
 }
